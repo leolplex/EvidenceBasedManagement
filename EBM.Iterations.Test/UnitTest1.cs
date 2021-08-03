f91aa87cd1631c926a8a47e1c919fd850b853f93
Daniel <raiga1234@gmail.com>
2021-08-03 10:32:27 -0500

    Add uit test to increase covergae

--- EBM.Iterations.Test/UnitTest1.cs
+++ EBM.Iterations.Test/UnitTest1.cs
@@ -1,4 +1,11 @@
+using System.Linq;
+using System.Collections;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace EBM.Iterations.Test
 {
@@ -12,7 +19,30 @@ namespace EBM.Iterations.Test
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+            new Claim(ClaimTypes.Name, "example name"),
+            new Claim(ClaimTypes.NameIdentifier, "1"),
+            new Claim("custom-claim", "example claim value"),
+            }, "mock"));
+
+            IdentityController identityController = new IdentityController();
+            identityController.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
+            var result = identityController.Get();
+            Assert.NotNull(result);
+
+            var JsonResult = result as JsonResult;
+            Assert.NotNull(JsonResult);
+
+            IEnumerable<Object> messages = JsonResult.Value as IEnumerable<Object>;
+            Assert.NotNull(messages);
+
+
+            List<Object> listMessages = messages.ToList();
+            Assert.AreEqual(listMessages.Count, 3);
         }
     }
 }
